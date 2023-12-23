fa157f5d2d1e3f240a1d93c0315fa59f851e0c4f
unknown <nastase4ka@mail.ru>
2023-12-23 11:16:47 +0300

    media hero section

--- scripts/hideElem.js
+++ scripts/hideElem.js
@@ -1,19 +1,37 @@
-const container = document.querySelector('.reviews__items_container');
-const button = document.querySelector('.button__light-revs');
+// const container = document.querySelector('.reviews__items_container');
+// const button = document.querySelector('.button__light-revs');
 
 
-container.style.height = '1200px';
-container.style.overflow = 'hidden';
+// container.style.height = '1200px';
+// container.style.overflow = 'hidden';
 
-button.addEventListener('click', function() {
-  if (container.style.height === '1200px') {
-    container.style.height = '2112px'; 
-    container.classList.add('no-gradient');
-    button.style.display = 'none'; 
+// button.addEventListener('click', function() {
+//   if (container.style.height === '1200px') {
+//     container.style.height = '2112px'; 
+//     container.classList.add('no-gradient');
+//     button.style.display = 'none'; 
 
+//   } else {
+//     container.style.height = '1000px'; 
+//     button.style.display = 'block'; 
+//     container.classList.remove('no-gradient');
+//   }
+// });
+const reviewsContainer = document.querySelector('.hero__reviews');
+const reviewsElements = reviewsContainer.querySelectorAll('.reviews__item');
+hideElement();
+
+window.addEventListener('resize', function (e) {
+  hideElement();
+}, true);
+
+function hideElement() {
+  let lastEl = reviewsElements[reviewsElements.length - 1];
+  if (window.screen.width <= 1080) {
+    lastEl.style.display = 'none';
+    lastEl.previousElementSibling.style.borderRight = '3px solid #FFB700';
   } else {
-    container.style.height = '1000px'; 
-    button.style.display = 'block'; 
-    container.classList.remove('no-gradient');
+    lastEl.style.display = 'block';
+    lastEl.previousElementSibling.style.borderRight = 'none';
   }
-});
+}
