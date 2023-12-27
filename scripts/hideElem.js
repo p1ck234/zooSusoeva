ca1fe9791e0ea3b2ebae285570a31a61f5a03a4b
unknown <nastase4ka@mail.ru>
2023-12-27 18:05:07 +0300

    added media reviews

--- scripts/hideElem.js
+++ scripts/hideElem.js
@@ -1,9 +1,28 @@
 const container = document.querySelector('.reviews__items_container');
 const button = document.querySelector('.button__light-revs');
-
-
-container.style.height = '1200px';
 container.style.overflow = 'hidden';
+
+if (this.window.screen.width <= 480) {
+  container.style.height = '2975px';
+} else if (window.screen.width <= 1080) {
+  container.style.height = '1370px';
+} else {
+  container.style.height = '1200px';
+}
+
+window.addEventListener('resize', function () {
+  if (this.window.screen.width <= 480) {
+    container.style.height = '2975px';
+  } else if (window.screen.width <= 1080) {
+    container.style.height = '1370px';
+  } else {
+    container.style.height = '1200px';
+  }
+
+});
+
+
+
 
 button.addEventListener('click', function () {
   if (container.style.height === '1200px') {
@@ -11,7 +30,16 @@ button.addEventListener('click', function () {
     container.classList.add('no-gradient');
     button.style.display = 'none';
 
-  } else {
+  } else if (container.style.height === '1370px') {
+    container.style.height = '100%';
+    container.classList.add('no-gradient');
+    button.style.display = 'none';
+  } else if (container.style.height === '2975px') {
+    container.style.height = '100%';
+    container.classList.add('no-gradient');
+    button.style.display = 'none';
+  }
+  else {
     container.style.height = '1000px';
     button.style.display = 'block';
     container.classList.remove('no-gradient');
